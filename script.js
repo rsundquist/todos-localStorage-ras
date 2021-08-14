6e39b8f0a70223af23c40ec62328f2be4cb01755
Rebecca Sundquist <rsundquist629@gmail.com>
2021-08-14 12:26:44 -0400

    updated html

---
+++ script.js
@@ -0,0 +1,26 @@
+// localStorage.setItem('myName', 'Rebecca')
+// localStorage.setItem('petName', 'Max')
+// localStorage.setItem('petAge', 8)
+
+// localStorage.setItem('petAge', 1)
+
+// var myName = localStorage.getItem('myName');
+
+// var petName = localStorage.getItem('petName');
+
+// var petAge = localStorage.getItem('petAge');
+
+// console.log(myName, petName, petAge);
+
+// //localStorage.clear()
+
+// localStorage.removeItem('petName')
+
+var myArray = [1,2,3,4,5]
+
+localStorage.setItem('myArray', myArray);
+
+consolelog (typeof localStorage.getItem('myArray'))
+
+
+
